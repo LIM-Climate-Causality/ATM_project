9ca4178a1bf09c93c005441366d3ac7abfa88812
Richard Alawode <richardalawode@Richards-MacBook-Air.local>
2024-10-07 09:54:45 +0200

    Updated file

--- download_awssi.sh
+++ download_awssi.sh
@@ -4,18 +4,20 @@
 # https://published.aer.com/era5_awssi/1950/era5_awssi_195001.nc
 # https://published.aer.com/era5_awssi/1950/era5_awssi_195012.nc
 
-# Base URL for downloading the files
-base_url="https://published.aer.com/era5_awssi"
+# Directory to save the downloaded files
+output_dir="era5_data/"
+mkdir -p $output_dir
 
-# Loop through the years (1950 to 2023)
+# Loop through the years (from 1950 to 2023)
 for year in {1950..2023}; do
-  # Loop through the months (01 to 12)
-  for month in {01..12}; do
-    # Construct the URL for the specific year and month
-    file_url="$base_url/$year/era5_awssi_${year}${month}.nc"
-
-    # Download the file using wget
-    echo "Downloading $file_url"
-    wget $file_url -P era5_data/
-  done
+    # Loop through the months (from 1 to 12)
+    for month in {1..12}; do
+        # Format month to ensure it has two digits (e.g., 01, 02, 03,...12)
+        month_padded=$(printf "%02d" $month)
+        
+        # Download the file using wget
+        file_url="https://published.aer.com/era5_awssi/${year}/era5_awssi_${year}${month_padded}.nc"
+        echo "Downloading $file_url"
+        wget -P $output_dir $file_url
+    done
 done
